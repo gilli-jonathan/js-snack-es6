cc3c6d969c022d0b74563362748ce25ed358702b
gilli-jonathan <gilli.jonathan97@gmail.com>
2025-10-17 14:36:09 +0200

    finish first snack

--- main.js
+++ main.js
@@ -6,13 +6,21 @@ Ogni oggetto descriverà una bici da corsa con le seguenti proprietà: nome e pe
 Stampare a schermo la bici con peso minore.
 */
 
-const bici_da_corsa = [
-  { nome: "Bici veloce", peso: 1800 },
-  { nome: "Bici arruginita", peso: 5 },
+const biciclette = [
+  { nome: "Bici veloce", peso: 100 },
+  { nome: "Bici arruginita", peso: 30 },
   { nome: "Bici rubata", peso: 13 },
   { nome: "Bici molto vecchia", peso: 59 },
   { nome: "Bici semplice", peso: 18 },
 ];
 
-Math.min(bici_da_corsa["peso"]);
-console.log(bici_da_corsa);
+let bici_leggera = biciclette[0];
+
+for (let i = 1; i < biciclette.length; i++) {
+  const bici = biciclette[i];
+
+  if (bici.peso < bici_leggera.peso) {
+    bici_leggera = bici;
+  }
+}
+console.log(bici_leggera);
